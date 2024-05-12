cfa70750f12e1e7c14fdbc6deb08925f848a970c
Diego Ramon <108502727+dianram@users.noreply.github.com>
2024-05-11 21:56:49 -0500

    Update Login.js

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -41,7 +41,7 @@ const Login = props => {
     enableReinitialize: true,
 
     initialValues: {
-      email: userLogin.email || "admin@themesbrand.com" || '',
+      email: userLogin.email || "Your Mail" || '',
       password: userLogin.password || "123456" || '',
     },
     validationSchema: Yup.object({
